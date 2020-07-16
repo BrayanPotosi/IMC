bec4bbb62d3155762a4fc84fff5e46949f17d796
Brayan Potosi <bp990308@hotmail.com>
2020-07-15 23:31:52 -0500

    Se transforma a enteros

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
-let estatura = document.getElementById('number_cm').value;
-let peso = document.getElementById('number_kg').value;
+let estatura = parseInt(document.getElementById('number_cm').value);
+let peso = parseInt(document.getElementById('number_kg').value);
 
-let calcular = parseInt(document.getElementById('buttonCalculate').addEventListener('click',funcalcular));
+let calcular = document.getElementById('buttonCalculate').addEventListener('click',funcalcular);
 
 function funcalcular(){
     console.log(estatura + peso)
