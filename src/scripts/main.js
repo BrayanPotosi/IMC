9c391a8df8fc7b819fb9bac3244d7b8dcfd29ba9
Brayan Potosi <bp990308@hotmail.com>
2020-07-16 14:23:32 -0500

    Se termina el desarrollo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,24 +2,24 @@ const calc = parseInt(document.querySelector('#buttonCalculate').addEventListene
 //               <>
 function funcalculate(){
     let result_text = ''
-    const height  = parseFloat(document.querySelector('#number_m').value);
-    const weight = parseFloat(document.querySelector('#number_kg').value);
-    const result =  (weight/(Math.pow(height , 2))).toFixed(1);
+    const height  = parseInt(document.querySelector('#number_m').value);
+    const height1 = (height/100).toFixed(2)
+    const weight1 = parseInt(document.querySelector('#number_kg').value);
+    const result1 =  (weight1/(Math.pow(height1 , 2))).toFixed(1);
     const finalText = document.querySelector("#final_text")
-
-    if(result <18.5){
-        result_text = `Su IMC es de ${result}, esto quiere decir que tienes un peso bajo`
+    
+    if(result1 <18.5){
+        result_text = `Su IMC es de ${result1}, esto quiere decir que tienes un peso bajo`
     }
-    else if(result >=18.5 && result <= 24.9){
-        result_text = `Su IMC es de ${result}, esto quiere decir que tienes un peso normal`
+    else if(result1 >=18.5 && result1 < 25){
+        result_text = `Su IMC es de ${result1}, esto quiere decir que tienes un peso normal`
     }
-    else if(result >=25 && result <= 25.9){
-        result_text = `Su IMC es de ${result}, esto quiere decir que tienes sobrepeso`
+    else if(result1 >=25.0 && result1 <= 29.9){
+        result_text = `Su IMC es de ${result1}, esto quiere decir que tienes sobrepeso`
     }
-    else if(result >= 30){
-        result_text = `Su IMC es de ${result}, esto quiere decir que estas obeso`
+    else if(result1 >= 30.0){
+        result_text = `Su IMC es de ${result1}, esto quiere decir que estas obeso`
     }
     console.log(result_text)
     finalText.innerHTML = result_text
-    
 }
