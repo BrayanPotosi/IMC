17a349f391629a1caba8d9ff8db17bdba7ed14a7
Brayan Potosi <bp990308@hotmail.com>
2020-07-16 12:40:00 -0500

    Se realizan cambios finales

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,25 @@
-const estatura = parseInt(document.querySelector('#number_cm').value);
-const peso = parseInt(document.querySelector('#number_kg').value);
+const calc = parseInt(document.querySelector('#buttonCalculate').addEventListener('click',funcalculate));
+//               <>
+function funcalculate(){
+    let result_text = ''
+    const height  = parseFloat(document.querySelector('#number_m').value);
+    const weight = parseFloat(document.querySelector('#number_kg').value);
+    const result =  (weight/(Math.pow(height , 2))).toFixed(1);
+    const finalText = document.querySelector("#final_text")
 
-const calcular = document.querySelector('#buttonCalculate').addEventListener('click',funcalcular);
-
-function funcalcular(){
-    console.log(estatura)
-    console.log(peso)
+    if(result <18.5){
+        result_text = `Su IMC es de ${result}, esto quiere decir que tienes un peso bajo`
+    }
+    else if(result >=18.5 && result <= 24.9){
+        result_text = `Su IMC es de ${result}, esto quiere decir que tienes un peso normal`
+    }
+    else if(result >=25 && result <= 25.9){
+        result_text = `Su IMC es de ${result}, esto quiere decir que tienes sobrepeso`
+    }
+    else if(result >= 30){
+        result_text = `Su IMC es de ${result}, esto quiere decir que estas obeso`
+    }
+    console.log(result_text)
+    finalText.innerHTML = result_text
+    
 }
